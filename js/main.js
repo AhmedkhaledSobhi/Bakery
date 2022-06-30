980a2017195716e2698f58f0685fe868d2b8e392
Ahmed khaled <105175448+ahmedKhaleSobhiKhattab@users.noreply.github.com>
2022-06-30 21:06:13 +0200

    return color navbar

--- js/main.js
+++ js/main.js
@@ -11,11 +11,12 @@ let woffset=$('#about').offset().top;
 
 $(window).scroll(function(){
     let wScroll=$(window).scrollTop();
-    if( wScroll > woffset -750)
+    if( wScroll > woffset -650)
     {
         $("#scrollNave").css('background-color','#969696');
         $("a.nav-link").css('color','black');
         $("#scrollImg").attr('src','images/bakery-color.png');
+        $('#slideUp').fadeIn(1000)
 
     }
     else
@@ -23,6 +24,8 @@ $(window).scroll(function(){
         $('#scrollNave').css('background-color','transparent');
         $("a.nav-link").css('color','#fff');
         $("#scrollImg").attr('src','images/bakery-light-1.png');
+        $('#slideUp').fadeOut(500)
+
     }
 
 })
